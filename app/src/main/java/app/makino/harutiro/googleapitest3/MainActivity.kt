fa90d3415ae49aee305f13c28c0cc4ff6fda6047
harutiro <harutiro2027@outlook.jp>
2021-05-05 21:07:14 +0900

    Initial commit

---
+++ app/src/main/java/app/makino/harutiro/googleapitest3/MainActivity.kt
@@ -0,0 +1,18 @@
+package app.makino.harutiro.googleapitest3
+
+import android.accounts.AccountManager
+import android.accounts.AccountManagerCallback
+import android.accounts.AccountManagerFuture
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+
+    }
+}
